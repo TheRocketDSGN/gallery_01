002207bd7a374e06336417403b45300d0801621d
TheRocketDSGN <88950705+TheRocketDSGN@users.noreply.github.com>
2022-08-04 17:29:50 +0100

    Update test.UE4.js

--- test.UE4.js
+++ test.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('test.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/TheRocketDSGN/gallery_01/16a214943be313742b4749ac6a8d8a7339ddd429/test.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
